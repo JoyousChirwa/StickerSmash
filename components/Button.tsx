8a35888869ef96635671ba3d7b58409b38ac5853
Joyous Chirwa <Joyouschirwa@outlook.com>
2025-09-06 12:47:26 +0200

    Added image picker fuction to index Added onpress functionality to the button Linked onpress from button to button view in index with image picker. Added useState to capture the input from image picker in index. Added usestate prop to image viewer to add input to the image tag. Arranged the imports

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,9 +4,10 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 type Props = {
   label: string;
   theme?: "primary"; 
-}
+  onPress?: () => void;
+};
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if ( theme === "primary") {
     return (
       <View style={[
@@ -15,7 +16,7 @@ export default function Button({ label, theme }: Props) {
       ]}>
         <Pressable 
           style={[styles.button, {backgroundColor: "#fff",}]}
-          onPress={() => alert("You pressed a button.")}
+          onPress={onPress}
         >
           <FontAwesome name="picture-o" color="#25292e" size={18} style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
